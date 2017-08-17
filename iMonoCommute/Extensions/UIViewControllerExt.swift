85f9da17e4cbb28fa62aa22b542f506c8b2cc15c
Ali•Gorithm <alisher.coder@gmail.com>
2017-08-16 20:18:05 -0400

    groupFeedTransitionImplemented

---
+++ iMonoCommute/Extensions/UIViewControllerExt.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewControllerExt.swift
+//  iMonoCommute
+//
+//  Created by Alisher Abdukarimov on 8/16/17.
+//  Copyright © 2017 MrAliGorithm. All rights reserved.
+//
+
+import Foundation
